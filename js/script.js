c5dba2eb399a982432d497e35b08e54c82c52d7a
danielevergani <dvergani94@gmail.com>
2021-04-09 16:25:22 +0200

    inserito dragscroll e line clamp

--- js/script.js
+++ js/script.js
@@ -2,8 +2,32 @@ var app = new Vue({
     el: "#app",
     data: {
         films: [],
-        searched: "" 
-    } 
-       
+        searched: "" ,
+        showOverview: "overview__clamp"
+    }, 
+    methods: {
+        search: function(){
+            axios.get("https://api.themoviedb.org/3/search/movie?api_key=56e179e57d4a7222dddc19a32b9ea0ee", {
+                params:{
+                    query: this.searched,
+                    language: "it-IT"
+                }
+            })
+            .then((results) => {
+                console.log(results.data.results);
+                this.films = results.data.results
+            });
+            
+        },
+        show: function(){
+            if(this.showOverview == "overview__clamp"){
+                this.showOverview = ""
+            }
+            else{
+                this.showOverview = "overview__clamp"
+            }
+            
+        }
+    }
     
 })
